33b29ea19a5b31d0c594dab8ef3c3b86d93d0d5c
Tyler Maschino <tylermaschino@gmail.com>
2018-05-12 07:57:46 -0400

    Added components folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Container from "./components/Container";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <p className="App-intro">
           Test
         </p>
+        <Container/>
       </div>
     );
   }
